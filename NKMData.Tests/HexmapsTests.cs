6ad1ac581cb7fa58d998defde194d4beb911e2fe
Krzysztof Ruczkowski <tojatos@gmail.com>
2019-05-25 19:14:56 +0200

    Hexmaps added

---
+++ NKMData.Tests/HexmapsTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace NKMData.Tests
+{
+    public class HexmapsTests
+    {
+        [Test]
+        public void AvaiableMapsNotEmpty()
+        {
+            var maps = new List<(string, string)>(Hexmaps.AvaiableMapsWithNames);
+			Assert.True(maps.Count > 0);
+        }
+    }
+}
---
+++ NKMData/Hexmaps.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace NKMData
+{
+    public class Hexmaps
+    {
+        public static readonly List<(string map, string name)> AvaiableMapsWithNames = new DirectoryInfo("HexMaps").GetFiles()
+            .Select(f => (File.ReadAllText(f.FullName), Path.GetFileNameWithoutExtension(f.Name))).ToList();
+
+    }
+}
